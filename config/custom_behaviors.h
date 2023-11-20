8dda33d09f4e8b72f1ebb7eaef43139e1324d7e7
maslias <marcel.liebreich@gmail.com>
2023-11-20 08:20:40 +0100

    try and error

--- config/custom_behaviors.h
+++ config/custom_behaviors.h
@@ -110,15 +110,22 @@ MAKE_MORPH_SHIFT(smart_shft_r, &sk RSHFT, &caps_word)
     };
 
 // urob smart num
-#define SMART_NUM &smart_num NUM 0
+#define SMART_NUM_LEFT &smart_num NUM_LEFT 0
 // MAKE_HTAP(smart_ger, &mo, &sl)
 MAKE_HTAP(smart_num, &mo, &num_dance)
 ZMK_BEHAVIOR(num_dance, tap_dance,
              tapping-term-ms = <300>;
-             bindings = <&num_word>, <&sl NUM>;  // reverse this for sticky-num on single tap
+             bindings = <&num_word>, <&sl NUM_LEFT>;  // reverse this for sticky-num on single tap
+             )
+#define SMART_NUM_RIGHT &smart_num NUM_RIGHT 0
+// MAKE_HTAP(smart_ger, &mo, &sl)
+MAKE_HTAP(smart_num, &mo, &num_dance)
+ZMK_BEHAVIOR(num_dance, tap_dance,
+             tapping-term-ms = <300>;
+             bindings = <&num_word>, <&sl NUM_RIGHT>;  // reverse this for sticky-num on single tap
              )
 &num_word {  // num-word, requires PR #1451
-    layers = <NUM>;
+    layers = <NUM_LEFT NUM_RIGHT>;
     continue-list = <BSPC DEL DOT COMMA PLUS MINUS STAR FSLH EQUAL>;
 };
 
@@ -128,7 +135,6 @@ MAKE_MORPH_SHIFT(smart_ctrl, &sk LCTRL, &kp LCTRL)
 
 MAKE_HTAP(lt_bs_del,&mo, &bs_del)
 MAKE_HTAP(hmt, &kp, &kp)
-MAKE_HTAP(mod_tap_sk, &kp LCTRL, &sk)
 
 
 
